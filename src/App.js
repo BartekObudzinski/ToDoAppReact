73ba20f9168abb4431f38fae5779a4c238d3a472
Holinice <obudzinskib@gmail.com>
2021-03-11 09:51:53 +0100

    fix: clear css class ToDoForm and added buttons to new component

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         todoTasks={todoTasks}
         setTodoTasks={setTodoTasks}
       />
+      <ToDoList />
     </div>
   );
 }
--- src/ToDoSingleTask.js
+++ src/ToDoSingleTask.js
@@ -3,6 +3,12 @@ const ToDoSingleTask = () => {
     <div className="singleTask">
       <p></p>
       <div className="buttonsContainer">
+        <button className="editButton">
+          <i class="material-icons">mode_edit</i>
+        </button>
+        <button className="doneButton">
+          <i class="material-icons">check</i>
+        </button>
         <button className="deleteButton">
           <i class="material-icons">clear</i>
         </button>
